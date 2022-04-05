4ab7ac6f1af67c4283cc7a3adcee915c53d301c3
bzeyveli <bekirzeyveli44@gmail.com>
2022-04-06 01:41:14 +0300

    Utility düzelendi ve kullanılmayan class kaldırıldı

--- emlakburada/src/main/java/com/patika/emlakburada/model/utilities/Utility.java
+++ emlakburada/src/main/java/com/patika/emlakburada/model/utilities/Utility.java
@@ -10,7 +10,7 @@ public class Utility {
     public static String getJwtFromRequest(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+            return bearerToken.substring(7);
         }
         return null;
     }
